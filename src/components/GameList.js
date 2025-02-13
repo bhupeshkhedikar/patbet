3425709bca94a3f85000d594a0c0b2a2e71c0cf2
bhupeshkhedikar <bhupesh.khedikar@extwebtech.in>
2025-02-13 18:51:47 +0530

    hide button samshya smadhan

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -61,11 +61,11 @@ const GameList = () => {
             <p className="text-center text-white mt-4">No announcement available</p>
           )
         }
-         <div className="help-button-container">
+         {/* <div className="help-button-container">
       <a href="https://forms.gle/dAv67wv4J7RfpjdK8" target="_blank" rel="noopener noreferrer" className="help-button">
       समस्या समाधान
       </a>
-    </div>
+    </div> */}
       {games.length > 0 ? (
         games.map((game) => <GameCard key={game.id} game={game} />)
       ) : (
