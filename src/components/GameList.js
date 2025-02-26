60a0b7851410aab82c9c9952ee64458435a1ad92
bhupeshkhedikar <bhupesh.khedikar@extwebtech.in>
2025-02-26 10:31:23 +0530

    anouncemnet chnaged

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -110,7 +110,7 @@ const GameList = () => {
         {activeTab === 0 ? (
           <div className="game-list">
            <h3 style={{ textAlign: "center", fontFamily: "Roboto, sans-serif" }}>
-  होडी के गेम सुबह १० बजे शुरू होंगे | कम से कम १०० रू. से बेटिंग शुरू होगी | 
+  होडी के गेम सुबह १० बजे से शुरू हो चुके हे | कम से कम १०० रू. से बेटिंग शुरू होगी | 
   अभी अपना अकाऊन्ट रिचार्ज करा के रखे
 </h3>
 
